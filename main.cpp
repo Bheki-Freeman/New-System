ebb020a57b7c9e4bee3972b4a0db1311ddc2f372
Bheki-Freeman <64682211+Bheki-Freeman@users.noreply.github.com>
2022-02-23 10:57:10 +0200

    Created the arrays and variables

--- main.cpp
+++ main.cpp
@@ -4,6 +4,28 @@ using namespace std;
 
 int main()
 {
-    
+    //variables
+    int final_mark = 0;
+    int lowest_mark = 100;
+    int highest_mark = 0;
+    char grade = 'X';
+    int number_of_students = 2;
+
+    string module_code = "AAIBP111";
+    string school = "Limkokwing University of Creative Technology";    
+    string faculty = "Information and Communications Technology";
+
+    //Arrays
+    string student_names[number_of_students];     
+    string student_surnames[number_of_students]; 
+    int student_ids[number_of_students];   
+    int test_one_marks[number_of_students];
+    int test_two_marks[number_of_students];
+    int tutorial_marks[number_of_students];
+    int project_marks[number_of_students];
+    int exam_marks[number_of_students];
+
+    //
+
     return 0;
 }
