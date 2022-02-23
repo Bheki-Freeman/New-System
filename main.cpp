d9f10fa49365fc2ca49527fe2857efa5c718ac40
Bheki-Freeman <64682211+Bheki-Freeman@users.noreply.github.com>
2022-02-23 11:14:35 +0200

    Made the first Display Page

--- main.cpp
+++ main.cpp
@@ -25,7 +25,35 @@ int main()
     int project_marks[number_of_students];
     int exam_marks[number_of_students];
 
-    //
+    //Display 
+    cout << school << endl;
+    cout << faculty << endl;
+    cout << module_code << endl;
+    cout << "--------------------------------------------" << endl;
+    cout << "---------- Student Report System -----------" << endl;
+    cout << "--------------------------------------------" << endl;
+    cout << "Choose what to do below" << endl;
+    cout << "1. Individual Student Report " << endl;
+    cout << "2. Class Report " << endl;
+    cout << "0. Exit" << endl;
+    int response;
+    cin >> response;
+    switch (response)
+    {
+    case 1:
+        cout << "Displaying Individual " << endl;
+        break;
+    case 2:
+        cout << "Displaying Class Report" << endl;
+        break;
+    case 0: 
+        exit(1);
+        break;
+    
+    default:
+        cout << "Wrong response" << endl;
+        break;
+    }
 
     return 0;
 }
